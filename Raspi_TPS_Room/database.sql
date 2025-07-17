f8c9a92d003a669c12c86f4ae6f765f3bb96e045
oticstps <oticsjetson@gmail.com>
2025-07-17 11:20:32 +0700

    Update database.sql

--- Raspi_TPS_Room/database.sql
+++ Raspi_TPS_Room/database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE nama_tabel (
+CREATE TABLE tb_area_cr1cr2 (
     idPrimary INT(11) NOT NULL AUTO_INCREMENT,
     date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     fasa VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -8,3 +8,66 @@ CREATE TABLE nama_tabel (
     voltage VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
     PRIMARY KEY (idPrimary)
 );
+
+
+CREATE TABLE tb_kub1_active_power (
+    idPrimary INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    power_meter VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    value VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    shift VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    day VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    week VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    month VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    year VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (idPrimary)
+);
+
+
+
+CREATE TABLE tb_kub1_pershift (
+    idPrimary INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    power_meter VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    value VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    consumption_energy VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    shift VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    day VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    week VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    month VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    year VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (idPrimary)
+);
+
+CREATE TABLE tb_kub1_total_kwh (
+    idPrimary INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    power_meter VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    value VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    shift VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    day VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    week VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    month VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    year VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (idPrimary)
+);
+
+
+CREATE TABLE name_line (
+    idPrimary INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    power_meter VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    value VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    shift VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    day VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    week VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    month VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    year VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (idPrimary)
+);
+
+
+
+
+
+
