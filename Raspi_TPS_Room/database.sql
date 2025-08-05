d30b09d5996158ad0b53d713fe9964c1400f0e2f
oticstps <oticsjetson@gmail.com>
2025-08-05 08:18:01 +0700

    Update database.sql

--- Raspi_TPS_Room/database.sql
+++ Raspi_TPS_Room/database.sql
@@ -1,17 +1,18 @@
 
 
-
-
 CREATE TABLE tb_area_cr1cr2 (
-    idPrimary INT(11) NOT NULL AUTO_INCREMENT,
-    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    fasa VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-    power VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-    energy VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-    current VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-    voltage VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-    PRIMARY KEY (idPrimary)
+  idPrimary INT(11) NOT NULL AUTO_INCREMENT,
+  date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  fasa VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  power VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  energy VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  current VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  voltage VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  total_energy VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (idPrimary)
 );
+
+
 
 
 CREATE TABLE tb_kub1_active_power (
